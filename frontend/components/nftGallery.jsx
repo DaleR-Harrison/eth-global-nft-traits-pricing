49bd3c33837e617129c2da564db7e791c5b63ecd
Micah <micah@bitcoin.com>
2023-04-14 23:04:34 +0900

    remove unused components and styling

--- frontend/components/nftGallery.jsx
+++ frontend/components/nftGallery.jsx
@@ -4,51 +4,28 @@ import { useAccount } from "wagmi";
 
 export default function NFTGallery({}) {
   const [nfts, setNfts] = useState();
-  const [walletOrCollectionAddress, setWalletOrCollectionAddress] =
-    useState("vitalik.eth");
-  const [fetchMethod, setFetchMethod] = useState("wallet");
   const [pageKey, setPageKey] = useState();
-  const [spamFilter, setSpamFilter] = useState(true);
   const [isLoading, setIsloading] = useState(false);
-  const { address, isConnected } = useAccount();
-  const [chain, setChain] = useState(process.env.NEXT_PUBLIC_ALCHEMY_NETWORK);
+  // const { address, isConnected } = useAccount();
+  const address = "0xdb5485C85Bd95f38f9def0cA85499eF67dC581c0";
 
-  const changeFetchMethod = (e) => {
+  const changeCollection = (e) => {
     setNfts()
     setPageKey()
-    switch (e.target.value) {
-      case "wallet":
-        setWalletOrCollectionAddress("vitalik.eth");
-
-        break;
-      case "collection":
-        setWalletOrCollectionAddress(
-          "0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e"
-        );
-        break;
-      case "connectedWallet":
-        setWalletOrCollectionAddress(address);
-        break;
-    }
-    setFetchMethod(e.target.value);
+    setWalletOrCollectionAddress(address);
   };
+
   const fetchNFTs = async (pagekey) => {
     if (!pageKey) setIsloading(true);
-    const endpoint =
-      fetchMethod == "wallet" || fetchMethod == "connectedWallet"
-        ? "/api/getNftsForOwner"
-        : "/api/getNftsForCollection";
+    const endpoint = "/api/getNftsForOwner";
     try {
       const res = await fetch(endpoint, {
         method: "POST",
         body: JSON.stringify({
-          address:
-            fetchMethod == "connectedWallet"
-              ? address
-              : walletOrCollectionAddress,
+          address: address,
           pageKey: pagekey ? pagekey : null,
-          chain: chain,
-          excludeFilter: spamFilter,
+          chain: "ETH_MAINNET",
+          excludeFilter: false,
         }),
       }).then((res) => res.json());
       if (nfts?.length && pageKey) {
@@ -71,56 +48,13 @@ export default function NFTGallery({}) {
 
   useEffect(() => {
     fetchNFTs();
-  }, [fetchMethod]);
-  useEffect(() => {
-    fetchNFTs();
-  }, [spamFilter]);
+  }, []);
 
   return (
     <div className={styles.nft_gallery_page}>
       <div>
         <div className={styles.fetch_selector_container}>
-          <h2 style={{ fontSize: "20px" }}>Explore NFTs by</h2>
-          <div className={styles.select_container}>
-            <select
-              defaultValue={"wallet"}
-              onChange={(e) => {
-                changeFetchMethod(e);
-              }}
-            >
-              <option value={"wallet"}>wallet</option>
-              <option value={"collection"}>collection</option>
-              <option value={"connectedWallet"}>connected wallet</option>
-            </select>
-          </div>
-        </div>
-        <div className={styles.inputs_container}>
-          <div className={styles.input_button_container}>
-            <input
-              value={walletOrCollectionAddress}
-              onChange={(e) => {
-                setWalletOrCollectionAddress(e.target.value);
-              }}
-              placeholder="Insert NFTs contract or wallet address"
-            ></input>
-            <div className={styles.select_container_alt}>
-              <select
-                onChange={(e) => {
-                  setChain(e.target.value);
-                }}
-                defaultValue={process.env.ALCHEMY_NETWORK}
-              >
-                <option value={"ETH_MAINNET"}>Mainnet</option>
-                <option value={"MATIC_MAINNET"}>Polygon</option>
-                <option value={"ETH_GOERLI"}>Goerli</option>
-                <option value={"MATIC_MUMBAI"}>Mumbai</option>
-              </select>
-            </div>
-            <div onClick={() => fetchNFTs()} className={styles.button_black}>
-              <a>Search</a>
-            </div>
-          </div>
-        </div>
+        </div> 
       </div>
 
       {isLoading ? (
@@ -129,27 +63,6 @@ export default function NFTGallery({}) {
         </div>
       ) : (
         <div className={styles.nft_gallery}>
-          {nfts?.length && fetchMethod != "collection" && (
-            <div
-              style={{
-                display: "flex",
-                gap: ".5rem",
-                width: "100%",
-                justifyContent: "end",
-              }}
-            >
-              <p>Hide spam</p>
-              <label className={styles.switch}>
-                <input
-                  onChange={(e) => setSpamFilter(e.target.checked)}
-                  checked={spamFilter}
-                  type="checkbox"
-                />
-                <span className={`${styles.slider} ${styles.round}`}></span>
-              </label>
-            </div>
-          )}
-
           <div className={styles.nfts_display}>
             {nfts?.length ? (
               nfts.map((nft) => {
