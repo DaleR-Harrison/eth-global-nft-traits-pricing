03616a291b374bbb603e90042e9bf5a96e23d94d
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-04-15 02:49:58 +0900

    adjust balance-tree template
    - include tokenId, percent and ceiling

--- hackend/src/balance-tree.js
+++ hackend/src/balance-tree.js
@@ -2,31 +2,33 @@
 // boilerplate code -> https://github.com/Uniswap/merkle-distributor/tree/master/src
 // modified to work with a simplified data object for project needs
 
-import MerkleTree from "./merkle-tree.js";
 import { utils } from "ethers";
+import MerkleTree from "./merkle-tree.js";
 
 export default class BalanceTree {
-    constructor(balances) {
+
+    constructor(_balances) {
         this.tree = new MerkleTree(
-            balances.map(({
-                account,
-                amount
-            }, index) => {
+            _balances.map(({ tokenId, percent, ceiling }, index) => {
                 return BalanceTree.toNode(
                     index,
-                    account,
-                    amount
+                    tokenId,
+                    percent,
+                    ceiling
                 );
             })
         );
     }
 
-    static verifyProof(index, account, amount, proof, root) {
+    verifyProof(index, tokenId, percent, ceiling, proof, root) {
+
         let pair = BalanceTree.toNode(
             index,
-            account,
-            amount
+            tokenId,
+            percent,
+            ceiling
         );
+
         for (const item of proof) {
             pair = MerkleTree.combinedHash(
                 pair,
@@ -37,37 +39,38 @@ export default class BalanceTree {
         return pair.equals(root);
     }
 
-    // keccak256(abi.encode(index, account, amount))
-    static toNode(index, account, amount) {
+    static toNode(index, tokenId, percent, ceiling) {
         return Buffer.from(
-            utils
-            .solidityKeccak256(
+            utils.solidityKeccak256(
                 [
                     "uint256",
-                    "address",
+                    "uint256",
+                    "uint256",
                     "uint256"
                 ],
                 [
                     index,
-                    account,
-                    amount
+                    tokenId,
+                    percent,
+                    ceiling,
                 ]
             )
             .substr(2),
             "hex"
-        )
+        );
     }
 
     getHexRoot() {
-        return this.tree.getHexRoot()
+        return this.tree.getHexRoot();
     }
 
-    getProof(index, account, amount) {
+    getProof(index, tokenId, percent, ceiling) {
         return this.tree.getHexProof(
             BalanceTree.toNode(
                 index,
-                account,
-                amount
+                tokenId,
+                percent,
+                ceiling
             )
         );
     }
