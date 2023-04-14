6d8ef2b358636ac72b96f7421fdce7e816d97bc7
Micah <micah@bitcoin.com>
2023-04-14 23:23:55 +0900

    separate nftcard into a component

--- frontend/components/gallery/nftGallery.jsx
+++ frontend/components/gallery/nftGallery.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import styles from "../styles/NftGallery.module.css";
 import { useAccount } from "wagmi";
+
+import styles from "../../styles/NftGallery.module.css"
+import NftCard from "../nftCard"; 
 
 export default function NFTGallery({}) {
   const [nfts, setNfts] = useState();
@@ -89,59 +91,6 @@ export default function NFTGallery({}) {
           </a>
         </div>
       )}
-    </div>
-  );
-}
-function NftCard({ nft }) {
-  return (
-    <div className={styles.card_container}>
-      <div className={styles.image_container}>
-        {nft.format == "mp4" ? (
-          <video src={nft.media} controls>
-            Your browser does not support the video tag.
-          </video>
-        ) : (
-          <img src={nft.media}></img>
-        )}
-      </div>
-      <div className={styles.info_container}>
-        <div className={styles.title_container}>
-          <h3>{nft.title}</h3>
-        </div>
-        <hr className={styles.separator} />
-        <div className={styles.symbol_contract_container}>
-          <div className={styles.symbol_container}>
-            <p>{nft.symbol}</p>
-
-            {nft.verified == "verified" ? (
-              <img
-                src={
-                  "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Twitter_Verified_Badge.svg/2048px-Twitter_Verified_Badge.svg.png"
-                }
-                width="20px"
-                height="20px"
-              />
-            ) : null}
-          </div>
-          <div className={styles.contract_container}>
-            <p className={styles.contract_container}>
-              {nft.contract?.slice(0, 6)}...
-              {nft.contract?.slice(38)}
-            </p>
-            <img
-              src={
-                "https://etherscan.io/images/brandassets/etherscan-logo-circle.svg"
-              }
-              width="15px"
-              height="15px"
-            />
-          </div>
-        </div>
-
-        <div className={styles.description_container}>
-          <p>{nft.description}</p>
-        </div>
-      </div>
     </div>
   );
 }
