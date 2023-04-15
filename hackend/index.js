528606ca15b34837c5d659efb7618389b2ff5b9e
DaleR-Harrison <dale@bitcoin.com>
2023-04-16 00:34:12 +0900

    remove unneccesarry import

--- hackend/index.js
+++ hackend/index.js
@@ -4,7 +4,6 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path'
-import { exec, spawn } from 'child_process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
