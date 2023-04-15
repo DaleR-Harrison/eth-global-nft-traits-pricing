92b2a93bc1a2dbe0e7fce03c0f6d1fb28cfc76b2
DaleR-Harrison <dale@bitcoin.com>
2023-04-15 15:03:12 +0900

    Add initial API

---
+++ hackend/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/getPricingData/:collectionName/:tokenId', (req, res) => {
+    const { collectionName, tokenId } = req.params;
+
+    if (!collectionName || !tokenId) {
+        return res.status(400).json({ error: 'Invalid parameters' });
+    }
+
+    const filePath = path.join(__dirname, 'trees-output/' + collectionName + '.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to read collection data' });
+        }
+
+        let collectionData;
+        try {
+            collectionData = JSON.parse(data);
+        } catch (e) {
+            return res.status(500).json({ error: 'Error parsing collection data' });
+        }
+
+        const pricingData = collectionData.PricingData.find((item) => item.tokenId === tokenId);
+
+        if (!pricingData) {
+            return res.status(404).json({ error: 'Token info for specified token ID was not found'});
+        }
+
+        res.json(pricingData);
+    });
+});
+
+//Listen to port
+const PORT = parseInt(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+    console.log('Server listening on port ${PORT}')
+});
