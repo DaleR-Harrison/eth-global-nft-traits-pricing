58bbd4671dfc49ea6623cfc55bbb84137d3af517
Micah <micah@bitcoin.com>
2023-04-15 20:49:34 +0900

    fix getpricingdata params

--- frontend/pages/api/getPricingData.js
+++ frontend/pages/api/getPricingData.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default async function getPricingData({ tokenName, tokenId }) {
+export default async function getPricingData(tokenName, tokenId) {
     const pricingDataUrl = `"https://third-anvil-383802.de.r.appspot.com/getPricingData/${tokenName}/${tokenId}"`
 
     try {
