3e4a6c1b331cf46ba25f14512ca5af95d160201f
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-04-15 14:31:00 +0900

    expand hardhat config

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -1,6 +1,31 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
-};
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 1337,
+            forking: {
+                url: "https://eth-mainnet.g.alchemy.com/v2/J3bTM7KLiYYwh8Ar_VBXuo-oLlGTx7od",
+                blockNumber: 16827927,
+                enabled: false
+            }
+        },
+    },
+    solidity: {
+        version: "0.8.19",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts"
+    }
+}
