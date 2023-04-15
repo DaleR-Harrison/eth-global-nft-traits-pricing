03fc520f7dd2d3239745874db4e121bc3ef3f78a
Micah <micah@bitcoin.com>
2023-04-15 11:01:40 +0900

    clean nftcard

--- frontend/components/nftCard.jsx
+++ frontend/components/nftCard.jsx
@@ -17,35 +17,6 @@ export default function NftCard({ nft }) {
             <h3>{nft.title}</h3>
           </div>
           <hr className={styles.separator} />
-          <div className={styles.symbol_contract_container}>
-            <div className={styles.symbol_container}>
-              <p>{nft.symbol}</p>
-  
-              {nft.verified == "verified" ? (
-                <img
-                  src={
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Twitter_Verified_Badge.svg/2048px-Twitter_Verified_Badge.svg.png"
-                  }
-                  width="20px"
-                  height="20px"
-                />
-              ) : null}
-            </div>
-            <div className={styles.contract_container}>
-              <p className={styles.contract_container}>
-                {nft.contract?.slice(0, 6)}...
-                {nft.contract?.slice(38)}
-              </p>
-              <img
-                src={
-                  "https://etherscan.io/images/brandassets/etherscan-logo-circle.svg"
-                }
-                width="15px"
-                height="15px"
-              />
-            </div>
-          </div>
-  
           <div className={styles.description_container}>
             <p>{nft.description}</p>
           </div>
